b2ffea4f450267c1f3b1fcd6112d0db5295cc94c
tomislavkordic <8417860+tomislavkordic@users.noreply.github.com>
2022-08-20 23:42:59 +0200

    Test github build and deploy action

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -219,7 +219,7 @@
   <footer class="main-footer">
     <strong>Copyright &copy; 2021 &nbsp;&nbsp;&nbsp;<a href="#">Mirko Miličić - završni rad ->  Implementacija programske podrške za praćenje aktivnosti parking senzora na pametnom parkingu</a></strong>
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0
+      <b>Version</b> 3.1.1
     </div>
   </footer>
 </div>
